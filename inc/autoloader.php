87db04db8d085951cb07abcb47e05305739227fb
Yang Wang <wang@stylelounge.de>
2017-03-20 22:14:29 +0100

    Add autoloader

---
+++ inc/autoloader.php
@@ -0,0 +1,33 @@
+<?php
+
+spl_autoload_register('wy_wp_nonce_autoloader');
+
+function wy_wp_nonce_autoloader($class_name) {
+  if (strpos($class_name, 'Wy_Wp_Nonce') === false) {
+    return;
+  }
+ 
+  $file_parts = explode( '\\', $class_name );
+    
+  $namespace = '';
+  for ($i = count($file_parts) - 1; $i > 0; $i--) {
+    $current = strtolower( $file_parts[ $i ] );
+    $current = str_ireplace( '_', '-', $current );
+      
+    if ( count( $file_parts ) - 1 === $i ) {
+      $file_name = "class-$current.php";
+    } else {
+      $namespace = '/' . $current . $namespace;
+    }
+  }
+    
+  $filepath  = trailingslashit(dirname(dirname( __FILE__)) . $namespace);
+  $filepath .= $file_name;
+  if (file_exists($filepath)) {
+    include_once($filepath);
+  } else {
+    wp_die(
+      esc_html( "The file attempting to be loaded at $filepath does not exist." )
+    );
+  }
+}
--- wy_wp_nonce.php
+++ wy_wp_nonce.php
@@ -8,6 +8,12 @@
  */
 
 use Wy_Wp_Nonce\Util; 
+/*
+include_once('util/class-nonce.php');
+include_once('util/class-url-nonce.php');
+include_once('util/class-field-nonce.php');
+*/
+require_once(trailingslashit(dirname( __FILE__)) . 'inc/autoloader.php' );
 
 global $url_nonce, $field_nonce;
 $url_nonce = new Util\Url_Nonce();
