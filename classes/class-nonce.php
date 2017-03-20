9c24b0d549af986a332c8611008d99010230ac7b
Yang Wang <wang@stylelounge.de>
2017-03-20 21:34:27 +0100

    Add base nonce class

---
+++ classes/class-nonce.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Wy_Wp_Nonce;
+
+class Nonce {
+  private $lifetime_sec = DAY_IN_SECONDS;
+  
+  public function __construct() {
+  }
+  
+  public function create_nonce($action = -1) {
+    $user = wp_get_current_user();
+    $uid = (int)$user->ID;
+    if (!$uid) {
+      $uid = apply_filters('nonce_user_logged_out', $uid, $action);
+    }
+
+    $token = wp_get_session_token();
+    $i = wp_nonce_tick();
+
+    return substr(wp_hash($i . '|' . $action . '|' . $uid . '|' . $token, 'nonce'), -12, 10);
+  }
+  
+  public function set_lifetime($lifetime_sec) {
+    $this->lifetime_sec = $lifetime_sec;
+  }
+  
+  private function nonce_tick() {
+    return ceil(time() / ($this->lifetime_sec / 2));
+  }
+}
---
+++ wy_wp_nonce.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+ * Plugin Name: Custom Nonce Plugin
+ * Description: A plugin that implement WP base nonce in OOP
+ * Version:     1.0
+ *
+ */
